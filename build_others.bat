1a71b359e5716e7db4d76eff9e4a5d316c4ecfcb
Andrew Dowsey <andrew.dowsey@manchester.ac.uk>
2014-12-08 00:53:54 +0000

    now build_others compiles release and debug builds

--- build_others.bat
+++ build_others.bat
@@ -1,39 +1,62 @@
 @echo off
 
-pushd "%~dp0/eigen-3.2.2"
-mkdir build
-cd build
-cmake -G"NMake Makefiles" -C"..\..\build.cmake" %* .. 2> nul
-nmake install
-popd
-
 pushd "%~dp0/zlib-1.2.8"
-mkdir build
-cd build
-cmake -G"NMake Makefiles" -C"..\..\build.cmake" %* ..
+mkdir build_release
+pushd build_release
+cmake -G"NMake Makefiles" -C"..\..\release.cmake" %* .. 2> nul
 nmake install
 popd
-
-@pushd "%~dp0/lpng1615"
-mkdir build
-cd build
-cmake -G"NMake Makefiles" -C"..\..\build.cmake" %* ..
+mkdir build_debug
+pushd build_debug
+cmake -G"NMake Makefiles" -C"..\..\debug.cmake" %* .. 2> nul
 nmake install
+popd
+rmdir /s /q build_release
+rmdir /s /q build_debug
+popd
+
+pushd "%~dp0/lpng1615"
+mkdir build_release
+pushd build_release
+cmake -G"NMake Makefiles" -C"..\..\release.cmake" %* ..
+nmake install
+popd
+mkdir build_debug
+pushd build_debug
+cmake -G"NMake Makefiles" -C"..\..\debug.cmake" %* ..
+nmake install
+popd
+rmdir /s /q build_release
+rmdir /s /q build_debug
 popd
 
 pushd "%~dp0/hdf5-1.8.14"
-mkdir build
-cd build
-cmake -G"NMake Makefiles" -C"..\..\build.cmake" %* ..
+mkdir build_release
+pushd build_release
+cmake -G"NMake Makefiles" -C"..\..\release.cmake" %* ..
 nmake install
 popd
-
-@pushd "%~dp0/spatialindex-src-1.8.5"
-mkdir build
-cd build
-cmake -G"NMake Makefiles" -C"..\..\build.cmake" %* ..
+mkdir build_debug
+pushd build_debug
+cmake -G"NMake Makefiles" -C"..\..\debug.cmake" %* ..
 nmake install
 popd
+rmdir /s /q build_release
+rmdir /s /q build_debug
+popd
 
-
+pushd "%~dp0/spatialindex-src-1.8.5"
+mkdir build_release
+pushd build_release
+cmake -G"NMake Makefiles" -C"..\..\release.cmake" %* ..
+nmake install
+popd
+mkdir build_debug
+pushd build_debug
+cmake -G"NMake Makefiles" -C"..\..\debug.cmake" %* ..
+nmake install
+popd
+rmdir /s /q build_release
+rmdir /s /q build_debug
+popd
 
